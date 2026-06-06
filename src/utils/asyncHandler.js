9f84203d1ffea1f17200ace6f7e37ef575902de7
Shubham Kumar Singh <shubham12an@gmail.com>
2026-06-06 21:57:16 +0530

    routes in our backend project using postman

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 // then and catch syntax
 
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
